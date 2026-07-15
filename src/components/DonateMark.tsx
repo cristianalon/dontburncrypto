f9e37df7c77945601ba41f73759d8f00f31dece3
DontBurnCrypto <cristianalonsosb@gmail.com>
2026-07-14 23:01:16 -0300

    adjust Donate mark alignment

--- src/components/DonateMark.tsx
+++ src/components/DonateMark.tsx
@@ -13,38 +13,40 @@ export default function DonateMark({ className }: DonateMarkProps) {
         className="mr-[-0.05em] h-[0.95em] w-[0.82em]"
         fill="none"
       >
-        {/* Outer coin circle */}
-        <circle
-          cx="21"
-          cy="21"
-          r="18"
-          stroke="#F7931A"
-          strokeWidth="2.5"
-        />
+        <g transform="translate(0 1.5)">
+          {/* Outer coin circle */}
+          <circle
+            cx="21"
+            cy="21"
+            r="18"
+            stroke="#F7931A"
+            strokeWidth="2.5"
+          />
 
-        {/* D shape */}
-        <path
-          d="M15 10V32"
-          stroke="#F7931A"
-          strokeWidth="3"
-          strokeLinecap="round"
-        />
+          {/* D shape */}
+          <path
+            d="M15 10V32"
+            stroke="#F7931A"
+            strokeWidth="3"
+            strokeLinecap="round"
+          />
 
-        <path
-          d="M15 10H22C28 10 32 14 32 21C32 28 28 32 22 32H15"
-          stroke="#F7931A"
-          strokeWidth="3"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
+          <path
+            d="M15 10H22C28 10 32 14 32 21C32 28 28 32 22 32H15"
+            stroke="#F7931A"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
 
-        {/* Bitcoin inspired bars */}
-        <path
-          d="M18 7V13M23 7V13M18 29V35M23 29V35"
-          stroke="#F7931A"
-          strokeWidth="2"
-          strokeLinecap="round"
-        />
+          {/* Bitcoin inspired bars */}
+          <path
+            d="M18 7V13M23 7V13M18 29V35M23 29V35"
+            stroke="#F7931A"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+        </g>
       </svg>
     </span>
   );
