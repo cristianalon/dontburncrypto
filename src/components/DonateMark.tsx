3edf84255c77b5dd682df7b1c27540740159ee02
DontBurnCrypto <cristianalonsosb@gmail.com>
2026-07-14 22:57:27 -0300

    Adjust Donate logo spacing

--- src/components/DonateMark.tsx
+++ src/components/DonateMark.tsx
@@ -10,7 +10,7 @@ export default function DonateMark({ className }: DonateMarkProps) {
     >
       <svg
         viewBox="0 0 42 42"
-        className="mr-[0.08em] h-[0.95em] w-[0.95em]"
+        className="mr-[-0.05em] h-[0.95em] w-[0.82em]"
         fill="none"
       >
         {/* Outer coin circle */}
@@ -38,7 +38,7 @@ export default function DonateMark({ className }: DonateMarkProps) {
           strokeLinejoin="round"
         />
 
-        {/* BTC inspired double bars */}
+        {/* Bitcoin inspired bars */}
         <path
           d="M18 7V13M23 7V13M18 29V35M23 29V35"
           stroke="#F7931A"
